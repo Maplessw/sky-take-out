9030bf9c563e0859ec5b7c75c745a51a440e48e1
maple <1910046133@qq.com>
2023-07-13 22:28:19 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -80,4 +80,15 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         shoppingCartMapper.insert(shoppingCart);
     }
 
+    /**
+     * 查看购物车
+     * @return
+     */
+    public List<ShoppingCart> showShoppingCart() {
+        //获取用户id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder().userId(userId).build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
